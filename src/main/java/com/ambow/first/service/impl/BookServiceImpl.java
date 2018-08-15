ba2580a9ae2e4ccdf6970865d8f0de8d165a5468
冬 <15326047083@163.com>
2018-08-15 20:39:18 +0800

    book:增删改查

--- src/main/java/com/ambow/first/service/impl/BookServiceImpl.java
+++ src/main/java/com/ambow/first/service/impl/BookServiceImpl.java
@@ -1,9 +1,144 @@
 package com.ambow.first.service.impl;
 
+import com.ambow.first.dao.BookMapper;
+import com.ambow.first.entity.Book;
 import com.ambow.first.service.BookService;
 
+import com.ambow.first.vo.BookTypeVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookMapper  bookMapper;
+
+    /**
+     *添加图书
+     * @param record
+     * @return
+     */
+    @Override
+    public int insert( Book record) {
+
+
+        return   bookMapper.insert(record);
+    }
+
+    /**
+     * 可选择增加
+     * @param record
+     * @return
+     */
+    @Override
+    public int insertSelective(Book record) {
+        return bookMapper.insertSelective(record);
+    }
+
+    /**
+     * 根据ID查询图书
+     * @param id
+     * @return
+     */
+
+    @Override
+    public Book selectByPrimaryKey(String id) {
+        return bookMapper.selectByPrimaryKey(id);
+    }
+
+    /**
+     * 修改图书
+     * @param record
+     * @return
+     */
+
+    @Override
+    public int updateByPrimaryKey(Book record) {
+        return bookMapper.updateByPrimaryKey(record);
+    }
+
+    /**
+     * 可选择修改
+     * @param record
+     * @return
+     */
+
+    @Override
+    public int updateByPrimaryKeySelective(Book record) {
+        return bookMapper.updateByPrimaryKeySelective(record);
+    }
+
+    /**
+     * 根据id删除
+     * @param id
+     * @return
+     */
+    @Override
+    public int deleteByPrimaryKey(String id) {
+        return bookMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 根据类型Id查询信息
+     * @param
+     * @return
+     */
+    @Override
+
+   public  List<BookTypeVo> getBookTypeVoList()
+    {
+        return bookMapper.getBookTypeVoList();
+    }
+
+    /**
+     * 根据类型ID
+     * 查询图书及类型
+     * @param typeId
+     * @return
+     */
+    @Override
+    public List<BookTypeVo> getBookTypeVoByTypeId(String typeId) {
+        return bookMapper.getBookTypeVoByTypeId(typeId);
+    }
+
+    /**
+     * 分页
+     *根据类型ID
+     * 查询图书及类型总数量
+     * @param typeId
+     * @return
+     */
+
+    @Override
+    public Integer getBookTypeVoByTypeIdNum(String typeId) {
+        return bookMapper.getBookTypeVoByTypeIdNum(typeId);
+    }
+
+    /**
+     * 总模糊查询
+     * @param blur
+     * @return
+     */
+    @Override
+    public List<Book> selectByLike(String blur) {
+
+        blur="%" + blur + "%";
+        System.out.println(blur);
+        return bookMapper.selectByLike(blur);
+    }
+
+    /**
+     * 基于分类下的模糊查询
+     * @param blur
+     * @return
+     */
+    @Override
+    public List<Book> getBookTypeVoByTypeIAndLike(String typeId,String blur) {
+        blur="%" + blur + "%";
+
+        return bookMapper.getBookTypeVoByTypeIAndLike(typeId,blur);
+    }
 }
---
+++ src/main/test/com/ambow/leiyuan/test/BookTest.java
@@ -0,0 +1,89 @@
+package com.ambow.leiyuan.test;
+
+import com.ambow.first.entity.Book;
+import com.ambow.first.service.BookService;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Date;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"/spring-mybatis.xml","/spring-service.xml"})
+public class BookTest {
+    @Autowired
+    private BookService bookService;
+
+    @org.junit.Test
+    public void testInsert() {
+
+        int i = 2;
+        while (i > 0) {
+            i--;
+            Book book = new Book();
+
+            book.setBookName("c++");
+            book.setAuthorName("刘");
+            book.setInfo("159");
+            book.setNum(2);
+            book.setPress("清华");
+            book.setTypeId("2");
+            book.setStatus(1);
+            book.setPublishDate(new Date());
+
+            book.setRemark("ddd");
+            bookService.insert(book);
+        }
+
+
+    }
+
+    @org.junit.Test
+    public void selectById() {
+
+        System.out.println(bookService.selectByPrimaryKey("3").toString());
+
+    }
+    @org.junit.Test
+    public void updateSelective() {
+
+        Book book=new Book();
+        book.setId("2");
+        book.setAuthorName("李");
+        System.out.println(bookService.updateByPrimaryKeySelective(book));
+
+    }
+
+    @org.junit.Test
+    public void deleteByid() {
+    bookService.deleteByPrimaryKey("3");
+    }
+
+    @org.junit.Test
+    public void getBookTypeVoList() {
+        System.out.println(bookService.getBookTypeVoList().toString());
+    }
+
+    @org.junit.Test
+    public void getBookTypeVoByTypeId() {
+        System.out.println(bookService.getBookTypeVoByTypeId("2").toString());
+    }
+    @org.junit.Test
+    public void getBookTypeVoByTypeIdNum() {
+        System.out.println(bookService.getBookTypeVoByTypeIdNum("2"));
+    }
+
+    @org.junit.Test
+    public void selectByLike() {
+        System.out.println(bookService.selectByLike("人").toString());
+    }
+
+    @org.junit.Test
+    public void getBookTypeVoByTypeIAndLike() {
+        System.out.println(bookService.getBookTypeVoByTypeIAndLike("2","b").toString());
+    }
+
+
+}
