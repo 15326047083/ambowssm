898575773672a375ccda329fbcb6cc4c6c0a6f43
caiyujing <935730876@qq.com>
2018-08-20 17:12:05 +0800

    蔡瑜靓：按时间数据统计

--- src/main/java/com/ambow/first/controller/DataAnalysisController.java
+++ src/main/java/com/ambow/first/controller/DataAnalysisController.java
@@ -2,17 +2,18 @@ package com.ambow.first.controller;
 
 import com.ambow.first.dao.DonateMapper;
 import com.ambow.first.service.DonateService;
+import com.ambow.first.vo.CountDateVo;
 import com.ambow.first.vo.DonateCountVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Controller
 @RequestMapping("/dataAnalysis")
@@ -20,31 +21,48 @@ public class DataAnalysisController {
 
     @Autowired
     private DonateService donateService;
+
     @RequestMapping("/toDataAnalysis")
-    public String toDataAnalysis(Model model){
-        List<DonateCountVo> list=new ArrayList<DonateCountVo>();
-        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd");
+    @ResponseBody
+    public CountDateVo toDataAnalysis(Model model, @RequestParam("start") String start, @RequestParam("end") String end) {
+
+        List<DonateCountVo> list = new ArrayList<DonateCountVo>();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         try {
-            Date date1 = simpleDateFormat.parse("2017-08-01");
-            Date date2=simpleDateFormat.parse("2018-09-10");
-            list=donateService.queryAllByDate(date1,date2);
+            Date date1;
+            Date date2;
+            if ("".equals(start)) {
+                start = null;
+                date1 = null;
+            } else {
+                date1 = simpleDateFormat.parse(start);
+            }
+            if ("".equals(end)) {
+                end = null;
+                date2 = null;
+            } else {
+                date2 = simpleDateFormat.parse(end);
+            }
+            list = donateService.queryAllByDate(date1, date2);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        int[] count=new int[list.size()];
-        Date[] date =new Date[list.size()];
-        int i=0;
-        for (DonateCountVo v:list){
-            count[i]=v.getCount();
-            date[i]=v.getDonateTime();
-        //    System.out.println(count[i]);
-          //  System.out.println(date[i]);
+        SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("yyyy-MM-dd");
+        int[] count = new int[list.size()];
+        String[] date = new String[list.size()];
+        int i = 0;
+        for (DonateCountVo v : list) {
+            count[i] = v.getCount();
+            date[i] = simpleDateFormat.format(v.getDonateTime());
+            System.out.println(date[i]);
+            i++;
+
         }
-        model.addAttribute("count",count);
-        model.addAttribute("date",date);
-
-
-        return "/dataAnalysis";
+        CountDateVo countDateVo = new CountDateVo();
+        countDateVo.setCount(count);
+        countDateVo.setDate(date);
+        System.out.println(list);
+        return countDateVo;
     }
 
 }
